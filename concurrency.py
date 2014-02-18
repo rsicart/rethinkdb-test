aa79a4415d5479b6ce7105b021e4d819ecd75f1a
Roger Sicart <roger@adserverpub.com>
2014-02-18 18:12:24 +0100

    Test method to call read and update. Random reads and writes.

--- concurrency.py
+++ concurrency.py
@@ -58,17 +58,21 @@ def read(dbhostname, dbname, dbtable, key = 'random'):
 		ReadCount.errors += 1
 
 
-def update(dbhostname, dbname, dbtable, key = 'all'):
+def update(dbhostname, dbname, dbtable, key = 'random'):
 	''' Update all users table
 	'''
 	try:
 		''' print "Thread update Start < key: {} >".format(key)
 		'''
 		rUp = r.connect(dbhostname, 28015, dbname)
-		if key is not None and key != 'all':
+		if key is not None and key == 'all':
+			r.db(dbname).table(dbtable).update({'address':'80.80.80.80'}).run(rUp)
+		elif key is not None and isinstance(key, (int, long)):
+			''' specific key '''
 			r.db(dbname).table(dbtable).get(key).update({'address':'80.80.80.{}'.format(randint(1,255))}).run(rUp)
 		else:
-			r.db(dbname).table(dbtable).update({'address':'80.80.80.80'}).run(rUp)
+			''' random '''
+			r.db(dbname).table(dbtable).get(randint(1,1000)).update({'address':'80.80.80.{}'.format(randint(1,255))}).run(rUp)
 
 		UpdateCount.hits += 1
 		''' print "Thread update finished."
@@ -93,6 +97,32 @@ def usage():
 	print "Usage: concurrency.py -h -r NUMBER_OF_READS"
 	print "Long options: --help --reads=NUMBER_OF_READS"
 
+def getHost():
+	dbhosts = [
+		"192.168.56.101", "192.168.56.102", "192.168.56.103", "192.168.56.104",
+		"192.168.56.105", "192.168.56.106", "192.168.56.107", "192.168.56.108",
+		"192.168.56.109", "192.168.56.110", "192.168.56.111", "192.168.56.112",
+		"192.168.56.113", "192.168.56.114", "192.168.56.115", "192.168.56.116",
+		"192.168.56.117", "192.168.56.118", "192.168.56.119", "192.168.56.120"
+	]
+	return dbhosts[randint(0,19)]
+
+def getDbConfig():
+	''' Returns a list with database configuration, including a random node
+	'''
+	dbname = "profiling"
+	dbtable = "users"
+	return [getHost(), dbname, dbtable]
+
+def test(test, threadList, counter, key = None):
+	''' Function to test sequential/random reads and writes
+	'''
+	if test in ('read', 'update'):
+		for i in range(0, counter):
+			threadList.append(Thread(target = globals()[test], name="test-{}-{}".format(test, i), args = getDbConfig() + [key]))
+	else:
+		usage
+
 
 def main(argv):
 	try:
@@ -101,17 +131,6 @@ def main(argv):
 		usage()
 		sys.exit(2)
 
-	''' DB Setup
-	'''
-	dbhosts = [
-		"192.168.56.101", "192.168.56.102", "192.168.56.103", "192.168.56.104",
-		"192.168.56.105", "192.168.56.106", "192.168.56.107", "192.168.56.108",
-		"192.168.56.109", "192.168.56.110", "192.168.56.111", "192.168.56.112",
-		"192.168.56.113", "192.168.56.114", "192.168.56.115", "192.168.56.116",
-		"192.168.56.117", "192.168.56.118", "192.168.56.119", "192.168.56.120"
-	]
-	dbname = "profiling"
-	dbtable = "users"
 
 	actions = {'key':None}
 	threadList = []
@@ -127,19 +146,17 @@ def main(argv):
 		elif opt in ('-r', '--reads'):
 			actions['reads'] = int(arg)
 		elif opt in ('-k', '--key'):
-			actions['key'] = int(arg)
+			actions['key'] = arg
 
 	if 'updates' in actions:
 		''' Update (launch thread)
 		'''
-		for i in range(0, actions['updates']):
-			threadList.append(Thread(target = update, name="concurrency-upd-{}".format(i), args = [dbhosts[randint(0,19)], dbname, dbtable, actions['key']]))
+		test('update', threadList, actions['updates'], actions['key'])
 
 	if 'reads' in actions:
 		''' Read (launch thread)
 		'''
-		for i in range(0, actions['reads']):
-			threadList.append(Thread(target = read, name="concurrency-rd-{}".format(i), args = [dbhosts[randint(0,19)], dbname, dbtable, actions['key']]))
+		test('read', threadList, actions['reads'], actions['key'])
 
 
 	''' Start running threads
@@ -150,7 +167,7 @@ def main(argv):
 
 	''' Reset environment (launch thread)
 	'''
-	threadList.append(Thread(target = reset, name="concurrency-rst", args = [dbhosts[0], dbname, dbtable]))
+	threadList.append(Thread(target = reset, name="concurrency-rst", args = getDbConfig()))
 	threadList[len(threadList)-1].start()
 
 
